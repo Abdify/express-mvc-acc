7572b1191596c29c24461caa3abdc44a6b36103c
Abdify <abdullah.fahimm08@gmail.com>
2022-08-24 16:08:29 +0600

    routes added

---
+++ routes/v1/tools.route.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const router = express.Router();
+
+
+// router.get("/", (req, res) => {
+//   res.send("tools found with id");
+// });
+
+
+// router.post("/", (req, res) => {
+//   res.send("tool added");
+// });
+
+
+router
+  .route("/")
+  /**
+   * @api {get} /tools All tools
+   * @apiDescription Get all the tools
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .get((req, res) => {
+    res.send("tools found");
+  })
+
+  /**
+   * @api {post} /tools save a tool
+   * @apiDescription Get all the tools
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
+  .post((req, res) => {
+    res.send("tool added");
+  });
+
+module.exports = router;
