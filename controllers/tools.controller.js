13c85d92c7fe1f7155fbe1f76f3c7512b20b8cc7
hasibulislam999 <hasib143sl@gmail.com>
2022-09-10 16:13:33 +0600

    modify tools controller

--- controllers/tools.controller.js
+++ controllers/tools.controller.js
@@ -1,123 +1,98 @@
-const { ObjectId } = require("mongodb");
-const { getDb } = require("../utils/dbConnect");
+// internal imports
+const {
+  postATool,
+  getAllTools,
+  getATool,
+  updateATool,
+  deleteATool,
+} = require("../services/tool.service");
 
-let tools = [
-  { id: 1, name: "Hammer" },
-  { id: 2, name: "Hammer2" },
-  { id: 3, name: "Hammer3" },
-];
-
+// routing to display all tools
 module.exports.getAllTools = async (req, res, next) => {
   try {
-    const { limit, page } = req.query;
-    const db = getDb();
+    const tools = await getAllTools();
 
-    // cursor => toArray(), forEach()
-    const tool = await db
-      .collection("tools")
-      .find({})
-      // .project({ _id: 0 })
-      // .skip(+page * limit)
-      // .limit(+limit)
-      .toArray();
-
-    res.status(200).json({ success: true, data: tool });
+    res.status(200).json({
+      success: true,
+      message: "OK",
+      data: tools,
+    });
   } catch (error) {
     next(error);
   }
 };
 
+// routing to insert a new tool
 module.exports.saveATool = async (req, res, next) => {
   try {
-    const db = getDb();
     const tool = req.body;
 
-    const result = await db.collection("tools").insertOne(tool);
+    const result = await postATool(tool);
     console.log(result);
 
-    if (!result.insertedId) {
-      return res.status(400).send({ status: false, error: "Something went wrong!" });
+    if (!result._id) {
+      return res
+        .status(400)
+        .send({ status: false, error: "Something went wrong!" });
     }
 
-    res.send({ success: true, message: `Tool added with id: ${result.insertedId}` });
+    res.status(201).send({
+      success: true,
+      message: "Created",
+    });
   } catch (error) {
     next(error);
   }
 };
 
+// routing to display specific tool
 module.exports.getToolDetail = async (req, res, next) => {
   try {
-    const db = getDb();
     const { id } = req.params;
 
-    if(!ObjectId.isValid(id)){
-      return res.status(400).json({ success: false, error: "Not a valid tool id."});
-    }
+    const tool = await getATool(id);
 
-    const tool = await db.collection("tools").findOne({_id: ObjectId(id)});
-
-    if(!tool){
-      return res.status(400).json({ success: false, error: "Couldn't find a tool with this id"});
-    }
-
-    res.status(200).json({ success: true, data: tool });
-    
+    res.status(200).json({
+      success: true,
+      message: "OK",
+      data: tool,
+    });
   } catch (error) {
     next(error);
   }
 };
 
+// routing to update an existing tool
 module.exports.updateTool = async (req, res, next) => {
   try {
-    const db = getDb();
     const { id } = req.params;
+    const body = req.body;
 
-    if (!ObjectId.isValid(id)) {
-      return res.status(400).json({ success: false, error: "Not a valid tool id." });
-    }
+    const tool = await updateATool(id, body);
+    console.log(tool);
 
-    const tool = await db.collection("tools").updateOne({ _id: ObjectId(id) }, { $set: req.body });
-
-    if (!tool.modifiedCount) {
-      return res.status(400).json({ success: false, error: "Couldn't update the tool" });
-    }
-
-    res.status(200).json({ success: true, message: "Successfully updated the tool" });
+    res.status(202).json({
+      success: true,
+      message: "Accepted",
+    });
   } catch (error) {
     next(error);
   }
 };
 
+// routing to delete a tool
 module.exports.deleteTool = async (req, res, next) => {
   try {
-    const db = getDb();
     const { id } = req.params;
 
-    if (!ObjectId.isValid(id)) {
-      return res.status(400).json({ success: false, error: "Not a valid tool id." });
-    }
+    const tool = await deleteATool(id);
+    console.log(tool);
 
-    const tool = await db.collection("tools").deleteOne({ _id: ObjectId(id) });
-
-    if (!tool.deletedCount) {
-      return res.status(400).json({ success: false, error: "Couldn't delete the tool" });
-    }
-
-    res.status(200).json({ success: true, message: "Successfully deleted the tool" });
+    res.status(200).json({
+      success: true,
+      message: "OK",
+    });
   } catch (error) {
     next(error);
   }
-};
-
-module.exports.test = async(req, res, next) => {
-  for (let i = 0; i < 100000; i++) {
-    const db = getDb();
-    db.collection("test").insertOne({name: `test ${i}`, age: i });
-  }
-};
-module.exports.testGet = async(req, res, next) => {
-  const db = getDb();
-
-  const result = await db.collection("test").find({ name: "test 99999" }).toArray();
-  res.json(result);
 };
