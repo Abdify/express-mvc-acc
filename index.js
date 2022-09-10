3a43f142bd6a5a7230219ef7573ed4ed5fd5dd4f
hasibulislam999 <hasib143sl@gmail.com>
2022-09-10 16:15:31 +0600

    refactorization done on as a whole

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
+// external imports
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const app = express();
-const port = process.env.PORT || 5000;
+
+// internal imports
 const dbConnect = require("./utils/dbConnect");
 const toolsRoutes = require("./routes/v1/tools.route.js");
 const errorHandler = require("./middleware/errorHandler");
-const { connectToServer } = require("./utils/dbConnect");
 
+// connection build-up
+const app = express();
+const port = process.env.PORT || 5000;
+
+// application level middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
-// app.use(viewCount);
+// connection with DB
+dbConnect();
 
-// Apply the rate limiting middleware to all requests
-// app.use(limiter);
-
-connectToServer((err) => {
-  if (!err) {
-    app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
-    });
-  } else {
-    console.log(err);
-  }
-});
-
+// external routing
 app.use("/api/v1/tools", toolsRoutes);
 
+// application level routing
 app.get("/", (req, res) => {
-  // res.send("Hello World");
-  // res.sendFile(__dirname + "/public/test.html");
   res.render("home.ejs", {
     id: 5,
     user: {
@@ -45,11 +38,10 @@ app.all("*", (req, res) => {
   res.send("NO route found.");
 });
 
+// global error handler
 app.use(errorHandler);
 
-process.on("unhandledRejection", (error) => {
-  console.log(error.name, error.message);
-  app.close(() => {
-    process.exit(1);
-  });
-});
+// enable connecton
+app.listen(port, ()=>{
+  console.log(`App listening on port: ${port}.`);
+})
